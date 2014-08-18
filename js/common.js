392e75bdc63c0537960b0b6362733f33f914a2cb
akella <akella.a@gmail.com>
2014-08-19 01:11:09 +0300

    done

--- js/common.js
+++ js/common.js
@@ -3,6 +3,25 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
+	$('.js-slick').slick({
+		  centerMode: true,
+		  centerPadding: '20px',
+		  slidesToShow: 5,
+		  responsive: [
+		    {
+		      breakpoint: 880,
+		      settings: {
+		        slidesToShow: 4
+		      }
+		    },
+		    {
+		      breakpoint: 790,
+		      settings: {
+		        slidesToShow: 3
+		      }
+		    }
+		  ]
+		});
 
 	console.log($('body').html());
 });
