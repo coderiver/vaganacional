5e0696a3cd62101b0774cd0cefdef9f785cb5642
hraboviyvadim <hraboviyvadim@gmail.com>
2014-09-04 12:33:22 +0300

    jobSeekerArea.html added

--- js/common.js
+++ js/common.js
@@ -43,28 +43,28 @@ head.ready(function() {
 
 // tabs
 
-	function tab() {
-       $(".js-tab").each(function(){
-         var tab_link = $(this).find("a");
-         var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
-         tab_cont.hide();
-         $(this).parents(".js-tab-group").find(".js-tab1").show();
-            tab_link.on("click", function() {
-             	var index = $(this).attr("href");
-             	tab_link.removeClass("is-active");
-             	tab_link.parent().removeClass("is-active");
-             	$(this).addClass("is-active");
-             	$(this).parent().addClass("is-active");
-             	tab_cont.hide();
-             	$(this).parents(".js-tab-group").find("."+index).show();
-                return false;
-            });
-        });
-    }
-    if ($(".js-tab-group").length) {
-    	tab();
-    };
-
+	//function tab() {
+    //   $(".js-tab").each(function(){
+    //     var tab_link = $(this).find("a");
+    //     var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+    //     tab_cont.hide();
+    //     $(this).parents(".js-tab-group").find(".js-tab1").show();
+    //        tab_link.on("click", function() {
+    //         	var index = $(this).attr("href");
+    //         	tab_link.removeClass("is-active");
+    //         	tab_link.parent().removeClass("is-active");
+    //         	$(this).addClass("is-active");
+    //         	$(this).parent().addClass("is-active");
+    //         	tab_cont.hide();
+    //         	$(this).parents(".js-tab-group").find("."+index).show();
+    //            return false;
+    //        });
+    //    });
+    //}
+    //if ($(".js-tab-group").length) {
+    //	tab();
+    //};
+    $('#demoTab').easyResponsiveTabs();
 // select
     function select() {
         $(".js-select").each(function(){
